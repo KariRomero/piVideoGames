c685fefa28dea47c912649df742584b5add9a0bc
KariRomero <karinaromero.1993@gmail.com>
2024-06-11 17:43:20 -0300

    new route actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -14,7 +14,7 @@ import {
 
 export const getVideogames= () => {
     return async function (dispatch){
-        const response = await axios.get('https://pi-video-games-back.vercel.app/videogames');
+        const response = await axios.get('https://pi-video-games-chi.vercel.app/videogames');
         const Videogames = response.data;
         dispatch({type: GET_VIDEOGAMES, payload: Videogames});
     }
@@ -22,7 +22,7 @@ export const getVideogames= () => {
 
 export const getVideogame = (id) => {
     return async function (dispatch){
-        const response = await axios.get(`https://pi-video-games-back.vercel.app/videogames/${id}`);
+        const response = await axios.get(`https://pi-video-games-chi.vercel.app/videogames/${id}`);
         const Videogame = response.data;
         dispatch ({type: GET_VIDEOGAME, payload:Videogame});
     }
@@ -30,7 +30,7 @@ export const getVideogame = (id) => {
 
 export const searchByName = (name) => {
     return async function (dispatch){
-        const response = await axios.get(`https://pi-video-games-back.vercel.app/videogames?name=${name}`);
+        const response = await axios.get(`https://pi-video-games-chi.vercel.app/videogames?name=${name}`);
         const Videogame = response.data;
         dispatch ({type: SEARCH_BY_NAME, payload: Videogame});    
     }
@@ -44,7 +44,7 @@ export const clearSearch = () => {
 
 export const getGenres = () => {
     return async function (dispatch) {
-        const response = await axios.get('https://pi-video-games-back.vercel.app/genres')
+        const response = await axios.get('https://pi-video-games-chi.vercel.app/genres')
         const genres = response.data
         dispatch({ type: GET_GENRES, payload: genres });        
     }
@@ -75,7 +75,7 @@ export const createVideogame = (videogame) => {
     return async (dispatch)=>{
         try {
             console.log(videogame);
-            const { data } = await axios.post(`https://pi-video-games-back.vercel.app/videogames`, videogame);
+            const { data } = await axios.post(`https://pi-video-games-chi.vercel.app/videogames`, videogame);
             return dispatch({
                 type: CREATE_VIDEOGAME,
                 payload: data
